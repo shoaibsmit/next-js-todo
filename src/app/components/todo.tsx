2016a930e236bb67d135809162512df9dbeb59e0
Shoaib Ahmed <153922877+shoaibsmit@users.noreply.github.com>
2024-11-02 01:11:12 +0500

    Add files via upload

---
+++ src/app/components/todo.tsx
@@ -0,0 +1,143 @@
+// "use client"
+// import { useAppDispatch, useAppSelector } from '@/lib/hooks';
+// import { addTodo } from '@/lib/slices/todoSlice';
+// import React, { useState } from 'react'
+
+
+// export default function Todo() {
+//     const [todo, setTodo] = useState('');
+//     const dispatch = useAppDispatch();
+//     const todos = useAppSelector(state => state?.todos?.todos)
+//     const add = () => {
+//         dispatch(addTodo(todo))
+//         setTodo('')
+//     }
+//     return (
+//         <div>
+//             <input type="text" value={todo} onChange={(e) => { setTodo(e?.target?.value) }} />
+//             <button onClick={add}>Add Todo</button>
+//             <ul>
+//                 {
+//                     todos?.map((text: string, index: number) => {
+//                         return <li key={index}>{text}</li>
+//                     })
+//                 }
+
+//             </ul>
+//         </div>
+//     )
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// "use client";
+// import { useAppDispatch, useAppSelector } from '@/lib/hooks';
+// import { addTodo, deleteTodo } from '@/lib/slices/todoSlice';
+// import React, { useState } from 'react';
+
+// export default function Todo() {
+//     const [todo, setTodo] = useState('');
+//     const dispatch = useAppDispatch();
+//     const todos = useAppSelector(state => state?.todos?.todos);
+
+//     const add = () => {
+//         dispatch(addTodo(todo));
+//         setTodo('');
+//     };
+
+//     const remove = (index: number) => {
+//         dispatch(deleteTodo(index));
+//     };
+
+//     return (
+//         <div>
+//             <input 
+//                 type="text" 
+//                 value={todo} 
+//                 onChange={(e) => setTodo(e.target.value)} 
+//             />
+//             <button onClick={add}>Add Todo</button>
+//             <ul>
+//                 {todos?.map((text: string, index: number) => (
+//                     <li key={index}>
+//                         {text}
+//                         <button onClick={() => remove(index)} style={{ marginLeft: '10px', color: 'red' }}>
+//                             Delete
+//                         </button>
+//                     </li>
+//                 ))}
+//             </ul>
+//         </div>
+//     );
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+"use client";
+import { useAppDispatch, useAppSelector } from '@/lib/hooks';
+import { addTodo, deleteTodo } from '@/lib/slices/todoSlice';
+import React, { useState } from 'react';
+
+export default function Todo() {
+    const [todo, setTodo] = useState('');
+    const dispatch = useAppDispatch();
+    const todos = useAppSelector(state => state?.todos?.todos);
+
+    const add = () => {
+        if (todo.trim()) {
+            dispatch(addTodo(todo));
+            setTodo('');
+        }
+    };
+
+    const remove = (index: number) => {
+        dispatch(deleteTodo(index));
+    };
+
+    return (
+        <div>
+            <input 
+                type="text" 
+                value={todo} 
+                onChange={(e) => setTodo(e.target.value)} 
+            />
+            <button onClick={add}>Add Todo</button>
+            <ul>
+                {todos?.map((text: string, index: number) => (
+                    <li key={index}>
+                        {text}
+                        <button onClick={() => remove(index)} style={{ marginLeft: '10px', color: 'red' }}>
+                            Delete
+                        </button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
